624f3645dd779c205006e30f8fc9a02c615efaf8
dedaCar <nikola89bgd@gmail.com>
2018-03-19 19:54:29 +0100

    Inital commit

---
+++ main.js
@@ -0,0 +1,86 @@
+var newDayBtn = document.getElementsByClassName('myBtn')[0],
+circle = document.getElementsByClassName('circle')[0],
+timeSpan = document.getElementById('timeSpan'),
+totalSpan = document.getElementById('totalSpan'),
+dateSpan = document.getElementById('dateSpan'),
+currentCigs,
+totalCigs,
+lastCig;
+
+setVars();
+newDayBtn.addEventListener('click',resetDay);
+circle.addEventListener('click',update);
+
+
+function update() {
+  lastCig = moment();
+
+  updateVars();
+  if (totalCigs == 1) {
+    setDate();
+  }
+  saveVars();
+  updateView();
+}
+
+function updateView() {
+  /*update circle */
+  circle.innerHTML = currentCigs;
+  /*uodate total */
+  totalSpan.innerHTML = totalCigs;
+  if (localStorage.startingDate)
+  dateSpan.innerHTML = localStorage.startingDate;
+  setInterval(() => {
+    if (lastCig) {
+      timeSpan.innerHTML = lastCig.fromNow();
+    }
+  },1000);
+
+}
+
+function setVars() {
+  if (localStorage.currentCigs) {
+    currentCigs = localStorage.currentCigs;
+  }else {
+    currentCigs = 0;
+  };
+  if (localStorage.totalCigs) {
+    totalCigs = localStorage.totalCigs;
+  }else {
+    totalCigs = 0;
+  };
+  if (localStorage.lastCig) {
+    lastCig = moment(localStorage.lastCig);
+  }else {
+    lastCig = 0;
+  }
+
+
+  updateView();
+}
+
+
+
+
+function resetDay() {
+  localStorage.currentCigs = 0;
+  setVars();
+}
+
+
+function saveVars() {
+  localStorage.currentCigs = currentCigs;
+  localStorage.totalCigs = totalCigs;
+  localStorage.lastCig = lastCig;
+  console.log(totalCigs);
+}
+
+function updateVars() {
+  currentCigs++;
+  totalCigs++;
+}
+
+function setDate() {
+  localStorage.startingDate = new Date().toDateString();
+  dateSpan.innerHTML = localStorage.startingDate;
+}
